f337239a6e502595e6fb7bf902b63e7ed5b5dec5
Guillaume <guillaume.boeur@gmail.com>
2020-07-13 16:16:15 +0200

    random mole move and score up

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log("Hello, world");
+const square = document.querySelectorAll(".square");
+const mole = document.querySelectorAll(".mole");
+const timeLeft = document.querySelector("#timeLeft");
+let score = document.querySelector("#score");
+
+let result = 0;
+
+function randomSquare() {
+    square.forEach((className) => {
+        className.classList.remove("mole");
+    });
+    let randomPosition = square[Math.floor(Math.random() * 9)];
+    randomPosition.classList.add("mole");
+
+    // Addsign the id of the randomPosition to hitPosition to use later
+    hitPosition = randomPosition.id;
+}
+
+square.forEach((id) => {
+    id.addEventListener("mouseup", () => {
+        if (id.id === hitPosition) {
+            result = result + 1;
+            score.textContent = result;
+        }
+    });
+});
+
+function moveMole() {
+    let timerId = null;
+    timerId = setInterval(randomSquare, 1000);
+}
+
+moveMole();
